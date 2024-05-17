ab027c3197fe6b53c9453876dae2fbed2890ecd9
Ana Julia <anajufg127@gmail.com>
2024-05-17 10:38:34 -0300

    Create ModeloProva.sql

---
+++ ModeloProva.sql
@@ -0,0 +1,64 @@
+
+-- ANA JULIA FERREIRA GARCIA
+-- DATA
+
+SELECT "ANA JULIA FERREIRA GARCIA";
+select sleep(5);
+
+-- Banco de Dados usado
+use Prova1A;
+
+--  1.
+select "Questao 1.1";
+
+select sleep(5);
+
+
+--  2.
+select "Questao 1.2";
+select sleep(5);
+
+
+--  3. 
+select "Questao 1.3";
+select sleep(5);
+
+
+--  4. 
+select "Questao 1.4";
+select sleep(5);
+
+--  5. 
+select "Questao 1.5";
+select sleep(5);
+
+--  6. 
+select "Questao 1.6";
+select sleep(5);
+
+--  7. 
+select "Questao 1.7";
+select sleep(5);
+
+--  8. 
+select "Questao 1.8";
+select sleep(5);
+
+-- Banco de Dados usado
+use EmpresaC;
+
+--  9. 
+select "Questao 2.1";
+select sleep(5);
+
+--  10. 
+select "Questao 2.2";
+select sleep(5);
+
+--  11. 
+select "Questao 2.3";
+select sleep(5);
+
+--  12. 
+select "Questao 2.4";
+select sleep(5);
